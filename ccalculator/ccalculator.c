5787fd6d70a7e0a5c58e906ad2a692692ad972c3
lipeRefosco] <luisfelipe.refosco@hotmail.com>
2023-11-14 01:50:29 -0300

    fix: infinit loop bug when the user enter a char instead of a integer

--- ccalculator/ccalculator.c
+++ ccalculator/ccalculator.c
@@ -1,6 +1,40 @@
 #include <stdio.h>
 
+enum Commands{
+    EXIT,
+    ADD,
+    SUB
+};
+
+int commandSelector() {
+    int tmp;
+
+    printf("Choose a command:\n");
+
+    if(scanf("%d", &tmp) != 1) {
+        while(getchar() != '\n');
+        return -1;
+    }
+
+    return tmp;
+}
+
 int main() {
-    printf("hello world!\n");
-    return 0;
+    int command;
+    enum Commands existCommand = EXIT;
+
+    while(command != existCommand) {
+        command = commandSelector();
+
+        switch(command) {
+            case EXIT:
+                printf("Bye...\n");
+                break;
+            default:
+                printf("Wrong selection!\n");
+        }
+
+    }
+
+    return EXIT;
 }
